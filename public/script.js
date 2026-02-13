14ce088c7c7e5147034da4a4cc61c0012e60a25c
Hexterty46 <efsuslusemih@gmail.com>
2026-02-13 02:04:05 +0100

    Final version for now
    #3 #2 #1

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
-const reserveButton = document.getElementById('openCloseReserve')
+const button = document.querySelector('#openCloseReserve')
+const reserveTeam = document.querySelector('.reserve-team')
+
+button.addEventListener('click', function() {
+    reserveTeam.classList.toggle('show')
+    console.log('werkt')
+})
